b0407270451ff3638962c5dab5c348dcef8ee86a
N.S.Devaraj <nsdevaraj@gmail.com>
2018-11-15 16:17:42 +0530

    webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,48 @@
-// This config is for building dist files
-const webpack = require('webpack');
-const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
 
-// noParse still leave `require('./locale' + name)` in dist files
-// ignore is better
-// http://stackoverflow.com/q/25384360
-function ignoreMomentLocale(webpackConfig) {
-  delete webpackConfig.module.noParse;
-  webpackConfig.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
-}
+// Imports
+const path = require('path');
+require("babel-register");
 
-function addLocales(webpackConfig) {
-  let packageName = 'antd-with-locales';
-  if (webpackConfig.entry['antd.min']) {
-    packageName += '.min';
-  }
-  webpackConfig.entry[packageName] = './index-with-locales.js';
-  webpackConfig.output.filename = '[name].js';
-}
-
-function externalMoment(config) {
-  config.externals.moment = {
-    root: 'moment',
-    commonjs2: 'moment',
-    commonjs: 'moment',
-    amd: 'moment',
-  };
-}
-
-const webpackConfig = getWebpackConfig(false);
-if (process.env.RUN_ENV === 'PRODUCTION') {
-  webpackConfig.forEach((config) => {
-    ignoreMomentLocale(config);
-    externalMoment(config);
-    addLocales(config);
-  });
-}
-
-module.exports = webpackConfig;
+const config = {
+  entry: './index-with-locales.js',
+    output: {
+    path: path.resolve(__dirname, './client'),
+    filename: 'bundle.js',
+    libraryTarget: "window",
+    library:"calendar",
+  },
+  resolve: { extensions: ['.tsx','.ts', '.js','.css','.less']},
+ 
+  // Loaders
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+         
+        }
+      },{
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader' // compiles Less to CSS
+        }]},
+     
+    { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+    ]
+  },
+  
+  // OPTIONAL
+  // Reload On File Change
+  watch: true,
+  
+  devtool: 'source-map'
+};
+// Exports
+module.exports = config;
