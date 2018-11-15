851f674c0f0d481e17067478a2a099a5b47678b1
N.S.Devaraj <nsdevaraj@gmail.com>
2018-11-15 20:18:17 +0530

    minor fixes

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,12 @@ const config = {
     }},
   plugins: [
     new BundleAnalyzerPlugin({generateStatsFile:true}),
+	new webpack.ContextReplacementPlugin(/components[/\\]locale[/-]provider$/, /en_US\.tsx|LocaleReceiver\.tsx/),
 	new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en-us)\.js/),
-	new webpack.ContextReplacementPlugin(/components[/\\]local[/-]provider$/, /en_US\.tsx|LocaleReceiver\.tsx/),
 	new webpack.ContextReplacementPlugin(/components[/\\]date[/-]picker[/\\]locale$/, /(en_US)\.tsx/),
 	new webpack.ContextReplacementPlugin(/components[/\\]calendar[/\\]locale$/, /(en_US)\.tsx/),
-	new webpack.ContextReplacementPlugin(/components[/\\]time[/-]picker[/\\]locale$/, /(en_US)\.js/),	
+	new webpack.ContextReplacementPlugin(/components[/\\]time[/-]picker[/\\]locale$/, /(en_US)\.tsx/),
+	new webpack.ContextReplacementPlugin(/rc[-]pagination[/\\]lib[/\\]locale$/, /(en_US)\.js/),	
 	new webpack.ContextReplacementPlugin(/calendar[/\\]lib[/\\]locale$/, /(en_US)\.js/)
   ],
   // Loaders
